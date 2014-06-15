acbc2d672af1bb2f8183630e1dd33911d60cd817
Jonatan Pedersen <jp@jonatanpedersen.com>
2014-06-15 22:24:36 +0200

    initial import

---
+++ src/DeviceOrientation/Hubs/DeviceOrientationHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace DeviceOrientation.Hubs
+{
+    public class DeviceOrientationEvent
+    {
+        public int Alpha { get; set; }
+        public int Beta { get; set; }
+        public int Gamma { get; set; }
+    }
+
+    public class DeviceOrientationHub : Hub
+    {
+        public void OnDeviceOrientation(string deviceId, float alpha, float beta, float gamma)
+        {
+            Clients.Group(deviceId).onDeviceOrientation(alpha, beta, gamma);
+        }
+
+        public void Subscribe(string deviceId)
+        {
+            Groups.Add(Context.ConnectionId, deviceId);
+        }
+
+        public void Unsubscribe(string deviceId)
+        {
+            Groups.Remove(Context.ConnectionId, deviceId);
+        }
+    }
+}
---
+++ src/DeviceOrientation/Scripts/deviceOrientation.js
@@ -0,0 +1,94 @@
+﻿var deviceOrientationApp = angular.module('deviceOrientationApp', [
+  'ngRoute'
+]);
+
+deviceOrientationApp.config(['$routeProvider',
+  function ($routeProvider) {
+      $routeProvider.
+        when('/home', {
+            templateUrl: 'home.html',
+            controller: 'HomeController'
+        }).
+        when('/device/:deviceId', {
+            templateUrl: 'device.html',
+            controller: 'DeviceController'
+        }).
+        otherwise({
+            redirectTo: '/home'
+        });
+  }]);
+
+deviceOrientationApp.controller('HomeController', function ($scope, $location) {
+    $scope.deviceId = '';
+    $scope.go = function() {
+        $location.path('/device/' + $scope.deviceId);
+    };
+});
+
+deviceOrientationApp.controller('DeviceController', function ($scope, $routeParams, signalRHubProxy) {
+    var deviceOrientationHubProxy = signalRHubProxy('deviceOrientationHub');
+
+    $scope.deviceId = $routeParams.deviceId;
+    $scope.deviceOrientation = { alpha: 1, beta: 1, gamma: 1 };
+    
+    window.ondeviceorientation = function (event) {
+        deviceOrientationHubProxy.proxy.invoke('onDeviceOrientation', $scope.deviceId, event.alpha, event.beta, event.gamma);
+    };
+
+    deviceOrientationHubProxy.proxy.on('onDeviceOrientation', function (alpha, beta, gamma) {
+        $scope.deviceOrientation = {
+            alpha: alpha,
+            beta: beta,
+            gamma: gamma
+        };
+
+        $scope.deviceRotation = 'rotate(' + ((beta - 90) * -1) + 'deg)';
+        $scope.$apply();
+    });
+
+    deviceOrientationHubProxy.connection.start().done(function() {
+        deviceOrientationHubProxy.proxy.invoke('subscribe', $scope.deviceId);
+    });
+});
+
+deviceOrientationApp.factory('signalRHubProxy', ['$rootScope', function ($rootScope) {
+    function signalRHubProxyFactory(hubName) {
+        var connection = $.hubConnection();
+        var proxy = connection.createHubProxy(hubName);
+
+        return {
+            on: function (eventName, callback) {
+                proxy.on(eventName, function (result) {
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback(result);
+                        }
+                    });
+                });
+            },
+            off: function (eventName, callback) {
+                proxy.off(eventName, function (result) {
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback(result);
+                        }
+                    });
+                });
+            },
+            invoke: function (methodName, callback) {
+                proxy.invoke(methodName)
+                    .done(function (result) {
+                        $rootScope.$apply(function () {
+                            if (callback) {
+                                callback(result);
+                            }
+                        });
+                    });
+            },
+            connection: connection,
+            proxy: proxy
+        };
+    };
+
+    return signalRHubProxyFactory;
+}]);
